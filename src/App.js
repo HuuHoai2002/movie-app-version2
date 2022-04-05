4467606cab1643598bbb79550a648821bf372217
Nghiem Huu Hoai <Huuhoai0511@gmail.com>
2022-04-05 17:49:35 +0700

    template project reactjs version 18 , update 05/04/2022, clone now....

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center flex-col font-semibold text-3xl text-red-500 mb-10">
+      <h1>Wellcome To Template Project ReactJS</h1>
     </div>
   );
-}
+};
 
 export default App;
